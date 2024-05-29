2109d6205b98ea42c1ed48fb8adfde6c917ff807
George Arthurovich Polevoy <agdars@gmail.com>
2024-05-29 12:38:59 +0000

    generation directive

---
+++ projects/my-lib/src/lib/directive-name4lib.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DirectiveName4libDirective } from './directive-name4lib.directive';
+
+describe('DirectiveName4libDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DirectiveName4libDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ projects/my-lib/src/lib/directive-name4lib.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[libDirectiveName4lib]',
+  standalone: true
+})
+export class DirectiveName4libDirective {
+
+  constructor() { }
+
+}
